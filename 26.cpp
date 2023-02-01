8eaf2194f80586ef5c810b978bc4f10d5143cc47
rishavmngo <rishavinmngo@gmail.com>
2023-02-01 16:32:02 +0530

    "26"

---
+++ 26.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <limits.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    int prevNumber = INT_MAX;
+    int ans = 0;
+
+    for (int i = 0, ip = 0; i < nums.size(); ++i) {
+
+      if (nums[i] != prevNumber) {
+        prevNumber = nums[i];
+        nums[ip++] = prevNumber;
+        ans++;
+      }
+    }
+
+    return ans;
+  }
+};
+
+int main(void) {
+  Solution sol;
+  vector<int> v = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+  cout << sol.removeDuplicates(v) << endl;
+
+  for (auto x : v) {
+    cout << x << " ";
+  }
+}
